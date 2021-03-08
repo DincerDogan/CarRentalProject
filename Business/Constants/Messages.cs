b0ff955bdc44378b83c23d3843eb3cf6ee9da9b3
cemozaydin <cem.ozaydin@gmail.com>
2021-03-08 23:59:17 +0300

    8 Mart 2021 - Ders10 Ödev4

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -23,6 +23,28 @@ namespace Business.Constants
         public static string ColorListed = "Renkler Listeleniyor...";
         public static string ColorDeleted = "Renk Silindi";
 
+        public static string UserAdded = "Kullanıcı Eklendi";
+        public static string FirstNameLastNameInvalid = "İsim veya Soyisim Girilmemiş";
+        public static string UserDeleted = "Kullanıcı Silindi";
+        public static string UserNotDeleted = "HATA. Kullanıcı Silinemedi";
+        public static string UserUpdated = "Kullanıcı Güncellendi";
+        public static string UsersListed = "Kullanıcılar Listeleniyor...";
+
+        public static string CustomerAdded = "Müşteri Eklendi";
+        public static string CustomerNotAdded = "HATA. Müşteri Eklenemedi";
+        public static string CustomerDeleted = "Müşteri Silindi";
+        public static string CustomerNotDeleted = "HATA. Müşteri Silinemedi";
+        public static string CustomerUpdated = "Müşteri Güncellendi";
+        public static string CustomersListed = "Müşteriler Listeleniyor...";
+
+        public static string RentalAdded = "Kiralama Bilgisi Eklendi";
+        public static string RentalAddedError = "Araç teslim edilmediği için tekrar kiraya verilemez";
+        public static string RentalUpdateReturnDate = "Araç Teslim Alındı";
+        public static string RentalUpdateReturnDateError = "Araç Teslim Alınmış";
+        public static string RentalUpdated = "Kiralama Bilgisi Güncellendi";
+        public static string RentalListed = "Kiralama Bilgileri Listeleniyor...";
+        public static string RentalDeleted = "Kiralama Bilgisi Silindi";
+
         public static string MaintenanceTime = "Sistem Bakımda";
         
     }
---
+++ Entities/DTOs/CustomerDetailDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.DTOs
+{
+    public class CustomerDetailDto
+    {
+        public int CustomerId { get; set; }
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+        public String UserLastName { get; set; }
+        public string CompanyName { get; set; }
+
+    }
+}
